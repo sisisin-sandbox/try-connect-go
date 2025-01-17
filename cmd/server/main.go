0fd63553f6e793e94d69d7873a49d6566feba456
sisisin <azsisisin@gmail.com>
2025-01-17 16:43:30 +0900

    add protovalidate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 
 	"connectrpc.com/connect"
+	"github.com/bufbuild/protovalidate-go"
 	greetv1 "github.com/sisisin-sandbox/try-connect-go/gen/greet/v1"
 	"github.com/sisisin-sandbox/try-connect-go/gen/greet/v1/greetv1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/protobuf/proto"
 )
 
 type GreetServer struct{}
@@ -43,11 +46,34 @@ func NewGreetServer() greetv1connect.GreetServiceHandler {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle(greetv1connect.NewGreetServiceHandler(NewGreetServer()))
+	interceptors := connect.WithInterceptors(NewValidateInterceptor())
+
+	mux.Handle(greetv1connect.NewGreetServiceHandler(NewGreetServer(), interceptors))
 
 	http.ListenAndServe(
 		"localhost:8181",
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
+}
+
+func NewValidateInterceptor() connect.UnaryInterceptorFunc {
+	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
+		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+			v, err := protovalidate.New()
+			if err != nil {
+				return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to initialize validator: %v", err))
+			}
+			msg, ok := req.Any().(proto.Message)
+			if !ok {
+				return nil, errors.New("failed to type assertion proto.Message")
+			}
+			if err = v.Validate(msg); err != nil {
+				return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("validation failed: %v", err))
+			}
+			return next(ctx, req)
+
+		})
+	}
+	return connect.UnaryInterceptorFunc(interceptor)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,20 @@ module github.com/sisisin-sandbox/try-connect-go
 go 1.22.6
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.3-20241127180247-a33202765966.1
 	connectrpc.com/connect v1.17.0
-	golang.org/x/net v0.23.0
+	github.com/bufbuild/protovalidate-go v0.8.2
+	golang.org/x/net v0.26.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53
-	google.golang.org/protobuf v1.35.1
+	google.golang.org/protobuf v1.36.3
 )
 
-require golang.org/x/text v0.14.0 // indirect
+require (
+	cel.dev/expr v0.18.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/google/cel-go v0.22.1 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240826202546-f6391c0de4c7 // indirect
+)
